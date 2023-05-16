14ca59b8d6ee36db6b6637bcba07a957b4221d47
davassayah <pn46hbx@eduvaud.ch>
2023-05-16 16:18:12 +0200

    Réalisation du footer
    Réalisation du frontend du footer conformément au concept de la maquette

---
+++ footer.php
@@ -0,0 +1,28 @@
+<?php
+
+?>
+
+<footer>
+    <nav class="navbar bg-dark navbar-expand-lg" data-bs-theme="dark">
+        <div class="container">
+            <a class="navbar-brand">Contact : </a>
+            <div class="collapse navbar-collapse">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" href="mailto:pn46hbx@eduvaud.ch" target="_blank">pn46hbx@eduvaud.ch</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="link-light" href="https://www.facebook.com" target="_blank">
+                            <img height="40em" src="./img/facebook.png" alt="edit">
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="link-light" href="https://www.instagram.com" target="_blank">
+                            <img height="35em" margin-top: 100px src="./img/instagram.png" alt="edit">
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            <a class="navbar-brand">Made with ♥ by David Assayah </a>
+        </div>
+    </nav>

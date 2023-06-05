a20773fbc30106cd944c33afc82201e4b4aaae6f
davassayah <113102402+davassayah@users.noreply.github.com>
2023-06-05 20:10:10 +0200

    Mise a jour du script sql

--- sql/EchangeDeCartesACollectionner.sql
+++ sql/EchangeDeCartesACollectionner.sql
@@ -21,6 +21,9 @@ SET time_zone = "+00:00";
 -- Base de données : `echangeDeCartesACollectionner`
 --
 
+CREATE DATABASE IF NOT EXISTS `echangeDeCartesACollectionner`;
+USE `echangeDeCartesACollectionner`;
+
 -- --------------------------------------------------------
 
 --
